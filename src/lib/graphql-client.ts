067b432ee5cc023f88b2fe25a1732359c9e228e2
Mushahid <raza.mushahid@rwit.io>
2026-04-27 17:00:55 +0530

    added the env file

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,7 +1,7 @@
 import { graphql } from "graphql";
 import { GraphQLClient } from "graphql-request";
 
-const endpoint = "http://localhost:3000/api/graphql";
+const endpoint = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/graphql`;
 
 export const graphqlClient = new GraphQLClient(endpoint, {
     headers: {}
